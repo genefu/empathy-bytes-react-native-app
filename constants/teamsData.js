f0c13c87f48bde01c03f0bf0853727ceddd9e23f
genefu <genesis_fu@yahoo.com>
2024-02-20 14:56:55 -0500

    Adds descriptions to teams data

--- constants/teamsData.js
+++ constants/teamsData.js
@@ -1,6 +1,10 @@
 export const teamData = [
-    {id: 1, name: "App Team", description: "this is the app team"},
-    {id: 2, name: "Web Team", description: "this is the web team"},
-    {id: 3, name: "VR/AR Team", description: "this is the VR/AR team"},
-    {id: 4, name: "Media Team", description: "this is the media team"}
+    {id: 1, name: "App Team",
+    description: "Our team is currently developing a cross-platform, mobile app using React Native to showcase our research and interactions with communities within Georgia Tech. We hope to explore a variety of ways to use multimedia technologies as a means to promote accessibility and cultivate empathy among users."},
+    {id: 2, name: "Web Team",
+    description: "Our team is currently enhancing the website to showcase our research. Our team is split into two subteams for Web Development and Design. We created the site using GatsbyJS and Decoupled Drupal. We also are experimenting with Web 3D using Three.js."},
+    {id: 3, name: "VR/AR Team",
+    description: "Our team develops Virtual and Augmented Reality experiences with the goal of fostering empathy and understanding. Through the use of 3D technologies, we aim to preserve communities and their artifacts, giving others the chance to see and learn more about them."},
+    {id: 4, name: "Media Team",
+    description: "The Media Team strives to explore new communities, document the work of all of our sub-teams, and promote our VIP in new and creative ways. When exploring communities, we often interview several members to find a clear view of what makes their community special. We also search for different artifacts that we can pass on to our different sub-teams so that they can discover new ways to preserve them in an engaging way."}
 ]
